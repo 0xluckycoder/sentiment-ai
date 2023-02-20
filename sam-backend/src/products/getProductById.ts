053b508eaecdfb0b26822bdd9a8f19ae698f5cc1
0xluckycoder <lakshanperera.dev@gmail.com>
2023-02-21 01:10:37 +0530

    getProductById functionality added

--- sam-backend/src/products/getProductById.ts
+++ sam-backend/src/products/getProductById.ts
@@ -1,7 +1,8 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context, APIGatewayProxyCallback } from 'aws-lambda';
 import { DynamoDBClient, QueryCommand } from '@aws-sdk/client-dynamodb';
 
-export const getProducts = async (
+
+export const getProductById = async (
     event: APIGatewayProxyEvent,
     context: Context,
     callback: APIGatewayProxyCallback
@@ -9,24 +10,47 @@ export const getProducts = async (
 
     try {
 
+        // extract id parameter
+        const productId = event.pathParameters!.id as string;
+
         const currentAuthUser = "24bef1a7-2816-48ed-bd09-bcbd07bb97e1";
 
         // grab the user from DB
         const dynamodbClient = new DynamoDBClient({
             region: `${process.env.Region}`,
         });
-
-        const getProductsCommand = new QueryCommand({
+        const getProductItemCommand = new QueryCommand({
             TableName: "product",
-            KeyConditionExpression: "user_id = :user_id",
+            KeyConditionExpression: "user_id = :user_id AND id = :id",
             ExpressionAttributeValues: {
                 ":user_id": {
                     S: currentAuthUser
+                },
+                ":id": {
+                    S: productId
                 }
             }
         });
-        const getItemResponse = await dynamodbClient.send(getProductsCommand);
+        const getItemResponse = await dynamodbClient.send(getProductItemCommand);
 
+        // throw error if user doesn't exits
+        if (!getItemResponse.Items) {
+            callback(null, {
+                statusCode: 404,
+                body: JSON.stringify({
+                    message: "no product found with given id",
+                }),
+            });
+        }
+
+        // construct the response
+        // const constructedResponse: any = {};
+        // const itemKeys = Object.keys(getItemResponse.Item as Object);        
+        // itemKeys.forEach(item => {
+        //     constructedResponse[item] = Object.values(getItemResponse!.Item![item])[0];
+        // });
+
+        // return the user if user is found
         return {
             statusCode: 200,
             body: JSON.stringify({
