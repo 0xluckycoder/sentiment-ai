4ca85ef08b1d508898167f52f34ef8700c9c5f84
0xluckycoder <lakshanperera.dev@gmail.com>
2023-02-27 00:49:36 +0530

    added schema validation to createReview

--- sam-backend/src/functions/reviews/createReview.ts
+++ sam-backend/src/functions/reviews/createReview.ts
@@ -3,7 +3,8 @@ import { v4 as uuidv4 } from 'uuid';
 import { ReviewRequest } from '../../types/custom';
 import { createReviewDB } from '../../database/review';
 import { errorHandler } from '../../utils/errorHandler';
-
+import { z } from "zod";
+import { customError } from '../../utils/customError';
 
 export const createReview = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
@@ -11,6 +12,20 @@ export const createReview = async (event: APIGatewayProxyEvent): Promise<APIGate
         const requestData = JSON.parse(event.body as string);
         const productId = 'product_1000';
         const userId = 'user_fed_1000';
+
+        // validate user input
+        const reviewInput = z.object({
+            title: z.string().max(20).optional(),
+            description: z.string().max(600),
+            star_rate: z.number().min(1).max(5).optional(),
+            occupation: z.string().max(30).optional()
+        });
+        const isValidData = reviewInput.safeParse(requestData);
+
+        // throw error if validation failed
+        if (!isValidData.success) {
+            throw customError('validation error', 'ValidationError');
+        }
 
         // construct and format the review object to be created
         const review: ReviewRequest = {
